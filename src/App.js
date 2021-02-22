3c4a4022075a4b2820ed707696b326c73119789b
shivamvora <shivampatel27102001@gmail.com>
2021-02-22 11:36:14 +0530

    import BroweserRouter and wrap navbar,home,about and contact component inside <Router>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <h1>CRUD Using Hooks with Bootstrap</h1>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Contact/>
+      </Router>
     </div>
   );
 }
