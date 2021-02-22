c06708c0f33ee87634892a4bf17117850b42c2a5
shivamvora <shivampatel27102001@gmail.com>
2021-02-22 20:44:40 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddUser from './components/AddUser'
+import EditUser from './components/EditUser'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <div>
-      shivam vora
+    <div className="App">
+      <h1>Nav</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/components/EditUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditUser = () => {
+    return (
+        <div>
+            <h1>Edit User</h1>
+        </div>
+    )
+}
+
+export default EditUser;
