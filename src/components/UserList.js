31d276eae8fd01ab8989218cdcd233874e53492a
shivamvora <shivampatel27102001@gmail.com>
2021-02-22 23:29:44 +0530

    update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,16 @@ import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 
 const UserList = () => {
     return (
-        <ListGroup>
+        <ListGroup className="mt-4">
+            <ListGroupItem className="d-flex">
+                <strong>User One</strong>
+                <div className="ml-auto">
+                    <Link className="btn btn-warning mr-1" to="/edit/1">Edit</Link>
+                    <Button color="danger" >Delete</Button>
+                </div>
+            </ListGroupItem>
+
+
             <ListGroupItem className="d-flex">
                 <strong>User One</strong>
                 <div className="ml-auto">
