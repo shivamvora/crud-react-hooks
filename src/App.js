86655ec988d91baa5e7914e609cec1f5bef9d185
shivamvora <shivampatel27102001@gmail.com>
2021-02-22 20:26:03 +0530

    uuid added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React from 'react';
-import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NotFound from './components/pages/NotFound';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-
-    <Router>
-      <div className="App">
-        <h1>CRUD Using Hooks with Bootstrap</h1>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
-
-      </div>
-    </Router>
-
-  );
+    <div>
+      shivam vora
+    </div>
+  )
 }
 
 export default App;
